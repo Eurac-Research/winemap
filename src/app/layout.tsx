a4c184fdb8a22c81402713199b76f5695f1f9262
tiacop <thomas.iacopino@eurac.edu>
2024-07-25 12:03:24 +0200

    feat: add google search console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
   title: "Winemap Europe by Eurac Research",
   description:
     "The Winemap provides a comprehensive overview of the 1,174 European wine regions which fall under the Protected Designation of Origin (PDO) label.",
+  verification: {
+    google: "OdAWzIPNr_gquodYDcLJpB5xjGfw0mJ1Mowe5Do9k6U",
+  },
 };
 export default function RootLayout({
   children,
