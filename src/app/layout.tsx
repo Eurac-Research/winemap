17b61382dc5c422df96f5c74c0599f6e46981172
tiacop <thomas.iacopino@eurac.edu>
2024-07-25 12:06:26 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ import PlausibleProvider from "next-plausible";
 import CookieConsent from "./components/CookieConsent";
 
 export const metadata: Metadata = {
-  title: "Winemap Europe by Eurac Research",
+  title: "Winemap Climate by Eurac Research",
   description:
-    "The Winemap provides a comprehensive overview of the 1,174 European wine regions which fall under the Protected Designation of Origin (PDO) label.",
+    "The Winemap Climate presents an overview of the climate change vulnerability of European Protected Designation of Origin (PDO) wine regions and is based on the integrated vulnerability index that was developed in the study 'Climate resilience of European wine regions'. ",
   verification: {
     google: "OdAWzIPNr_gquodYDcLJpB5xjGfw0mJ1Mowe5Do9k6U",
   },
